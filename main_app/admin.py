1d7f5197927c9b3df33624d9108ec6b756693d2c
Wan Aqim <wanaqim@gmail.com>
2023-12-14 16:31:05 +0800

    add model register to admin site

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser, Doctor, Patient, Appointment, MedicalRecords
 
-# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
+admin.site.register(MedicalRecords)
