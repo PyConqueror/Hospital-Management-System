e63e50ac06c3001931f02ff36b909c3c53895dd9
Wan Aqim <wanaqim@gmail.com>
2023-12-16 15:56:19 +0800

    minor fix to hash password when creating user account on admin site

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Doctor, Patient, Appointment, MedicalRecords
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'role', 'is_staff', ]
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional info', {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional info', {
+            'classes': ('wide',),
+            'fields': ('role',),
+        }),
+    )
+
 admin.site.register(Doctor)
 admin.site.register(Patient)
 admin.site.register(Appointment)
 admin.site.register(MedicalRecords)
+admin.site.register(CustomUser, CustomUserAdmin)
