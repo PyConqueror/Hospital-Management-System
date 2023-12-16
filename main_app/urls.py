f8c1377cc9e36b571b5dc4de26ae863ac2a3aada
Wan Aqim <wanaqim@gmail.com>
2023-12-16 13:00:40 +0800

    add new urls for user to add and edit appointments

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,18 +3,19 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('dashboard/', views.set_dashboard, name='dashboard'),
     path('patient/dashboard/', views.patient_dashboard, name='patient_dashboard'),
+    path('appointment/<int:pk>/', views.AppointmentDetail.as_view(), name='appointment_detail'),
+    path('appointment/<int:pk>/edit/', views.AppointmentUpdate.as_view(), name='appointment_edit'),
+    path('patient/appointment_request/', views.AppointmentRequest.as_view(), name='appointment_request_create'),
     # path('patient/medical_history/', views.patient_medical_history, name='patient_medical_history'),
     # path('patient/profile/', views.patient_profile, name='patient_profile'),
-    # path('patient/appointment_request/', views.appointment_request, name='appointment_request'),
     path('doctor/dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
     # path('doctor/my_patients/', views.my_patients, name='my_patients'),
-    # path('admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
-    # path('admin/manage_doctors/', views.manage_doctors, name='manage_doctors'),
-    # path('admin/appointment_requests/', views.appointment_requests, name='appointment_requests'),
+    path('manager/dashboard/', views.manager_dashboard, name='manager_dashboard'),
+    # path('manager/manage_doctors/', views.manage_doctors, name='manage_doctors'),
+    # path('manager/appointment_requests/', views.appointment_requests, name='appointment_requests'),
     path('signup/patient/', views.patient_signup, name='patient_signup'),
     path('signup/doctor/', views.doctor_signup, name='doctor_signup'),
-    path('accounts/login/dashboard', views.set_dashboard, name='dashboard')
+    path('accounts/login/dashboard', views.set_dashboard, name='dashboard'),
     ]
  
